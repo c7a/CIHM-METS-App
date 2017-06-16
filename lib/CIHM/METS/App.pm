68131e9cf51c589320e35867565c469925faa331
Russell McOrmond <russell.mcormond@canadiana.ca>
2017-06-16 10:37:18 -0400

    Bumped version for distribution

--- lib/CIHM/METS/App.pm
+++ lib/CIHM/METS/App.pm
@@ -11,11 +11,11 @@ CIHM::METS::App - Command-line tool for generating/manipulating components of ME
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 AUTHOR
 
